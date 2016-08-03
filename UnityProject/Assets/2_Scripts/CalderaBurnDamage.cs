bba2c2dafe844e9bcebb96677f213957a7aa35df
banjoe900 <josephbyrnes@hotmail.com>
2016-08-03 17:52:47 +1000

    Caldera dot and prefab folder cleanup

--- UnityProject/Assets/2_Scripts/CalderaBurnDamage.cs
+++ UnityProject/Assets/2_Scripts/CalderaBurnDamage.cs
@@ -2,14 +2,50 @@
 using System.Collections;
 
 public class CalderaBurnDamage : MonoBehaviour {
+    
+    private bool isBurning = false;
+    public float dotTimer = 1;
+    private float timer;
+    public int numberOfTicks = 3;
+    private int currentTick;
+    public float tickDamage = 1;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    if(Time.time > timer && currentTick < numberOfTicks)
+        {
+            gameObject.SendMessage("TakeDmg", tickDamage);
+            currentTick++;
+            timer += dotTimer;
+        }
 	}
+
+    public bool IsBurning
+    {
+        get
+        {
+            return isBurning;
+        }
+
+        set
+        {
+            if (value)
+            {
+                timer = Time.time;
+                currentTick = 0;
+            }
+            else
+            {
+                currentTick = numberOfTicks;
+            }
+            isBurning = value;
+        }
+    }
 }
