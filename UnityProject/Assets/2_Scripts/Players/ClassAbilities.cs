344607d203ffe61695964ddae870281c1e9aebff
banjoe900 <josephbyrnes@hotmail.com>
2016-08-28 22:44:56 +1000

    change IsInvulnerable to character

--- UnityProject/Assets/2_Scripts/Players/ClassAbilities.cs
+++ UnityProject/Assets/2_Scripts/Players/ClassAbilities.cs
@@ -373,8 +373,4 @@ public class ClassAbilities : Character {
             return new Vector3(transform.position.x, transform.position.y+1, transform.position.z);
         }
     }
-
-    public virtual bool IsInvulnerable() {
-        return false;
-    }
 }
